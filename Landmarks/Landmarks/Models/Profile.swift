586838d3b3892d39ef88b4deb5fad7be90d5985e
민성홍 <sunghong32@naver.com>
2021-07-09 14:31:57 +0900

    Composing Complex Interfaces & Working with UI Controls

---
+++ Landmarks/Landmarks/Models/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 민성홍 on 2021/07/08.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "sunghong32")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
